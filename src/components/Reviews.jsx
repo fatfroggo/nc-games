4d2ceaad1cd7b4c84c8b816be1eac907c9a17a1d
fatfroggo <lilybaird1@gmail.com>
2022-12-05 15:10:33 +0000

    updated following comments

--- src/components/Reviews.jsx
+++ src/components/Reviews.jsx
@@ -2,18 +2,7 @@ import { useEffect, useState } from "react";
 import { getReviews } from "../api";
 
 const Reviews = () => {
-  const [reviewsList, setReviewsList] = useState([
-    {
-      title: "One Night Ultimate Werewolf",
-      designer: "Akihisa Okui",
-      owner: "happyamy2016",
-      review_img_url:
-        "https://images.pexels.com/photos/5350049/pexels-photo-5350049.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
-      category: "hidden-roles",
-      created_at: 1610964101251,
-      votes: 5,
-    },
-  ]);
+  const [reviewsList, setReviewsList] = useState([]);
 
   useEffect(() => {
     getReviews().then((reviews) => {
