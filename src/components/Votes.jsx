d185a50c0c98322d338a77315a1d05b27b656d61
fatfroggo <lilybaird1@gmail.com>
2022-12-06 13:58:12 +0000

    allow only one vote/devote

--- src/components/Votes.jsx
+++ src/components/Votes.jsx
@@ -2,25 +2,33 @@ import { useEffect, useState } from "react";
 import { addVotes, removeVotes } from "../api";
 
 const Votes = ({ review_id, currentVotes, setVotes }) => {
+
+    const [addVoteClicked, setAddVoteClicked] = useState(false)
+    const [deleteVoteClicked, setDeleteVoteClicked] = useState(false)
+
   const handleAddVotes = () => {
     setVotes(currentVotes + 1);
     addVotes(review_id);
+    setAddVoteClicked(true)
+    setDeleteVoteClicked(false)
   };
 
   const handleRemoveVotes = () => {
     setVotes(currentVotes - 1)
     removeVotes(review_id)
+    setDeleteVoteClicked(true)
+    setAddVoteClicked(false)
   }
   return (
     <div className="voting-buttons">
-      <button className="button"
+      <button className="button" disabled={addVoteClicked}
         onClick={() => {
           handleAddVotes();
         }}
       >
         👍
       </button>
-      <button className="button"
+      <button className="button" disabled={deleteVoteClicked}
         onClick={() => {
           handleRemoveVotes();
         }}
